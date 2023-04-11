40ead4327d4022bd193126f34b6fca1a63de6451
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-04-11 11:29:37 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
+         // obj.name = "Mohd Talib";
+        // System.out.println(obj.name);
         obj.setName("Mohd Talib");
         System.out.println(obj.getName());
-        // obj.name = "Mohd Talib";
-        // System.out.println(obj.name);   
+     
     }
 }
